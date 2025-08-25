0a72a6e22ce04dbf2e9b0a74c69cc673de11dd95
RedStarOverPlank <2900517834@qq.com>
2025-08-25 20:06:22 +0800

    雨天没有小动物的声音

--- src/main/java/com/plank/terra_atmos/atmos/WildLife.java
+++ src/main/java/com/plank/terra_atmos/atmos/WildLife.java
@@ -33,7 +33,7 @@ public class WildLife {
             BlockPos blockPos = BlockPos.containing(px + (double)random.nextInt(17) - 8, py + (double)random.nextInt(17) - 8, pz + (double)random.nextInt(17) - 8);
             int brightness = level.getBrightness(LightLayer.SKY, blockPos);
             float magnification = 0.4f;
-            if (level.dimension().location().getPath().equals("overworld") && py >= 64 && py <= 128 && brightness > 5)
+            if (level.dimension().location().getPath().equals("overworld") && !level.isRaining() && py >= 64 && py <= 128 && brightness > 5)
                 moodiness += (float) brightness * magnification / 15;
             else moodiness = Math.max(moodiness - (magnification / 20), 0.0f);
             if (moodiness >= 1.0F) {
